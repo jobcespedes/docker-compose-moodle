a85e59d3d228b359ff3430ceb4096e1a5aec53ea
Job Céspedes Ortiz <jobcespedes@gmail.com>
2020-07-08 13:44:45 -0600

    test: check exit codes

--- test/scripts/script.sh
+++ test/scripts/script.sh
@@ -5,5 +5,11 @@
 set -e
 
 docker-compose up -d
+exit_codes=$(docker-compose ps -q | xargs docker inspect -f '{{ .Name }} exited with status Exit {{ .State.ExitCode }}')
+errors=$(echo "$exit_codes" | egrep -v "Exit 0" | wc -l)
+if (($errors > 0)); then
+    echo "$exit_codes" | egrep -v "Exit 0"
+    exit 1
+fi
 
 set +e
