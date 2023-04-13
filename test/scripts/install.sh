a611c4aea131ff4c5238cd328377dd918ab0ecfe
Job Céspedes Ortiz <jobcespedes@gmail.com>
2023-04-13 11:29:42 -0600

    doc: fix moodle 4.1 branch
    Fixes #15

--- test/scripts/install.sh
+++ test/scripts/install.sh
@@ -4,6 +4,6 @@
 # Fail if one of the commands of this script fails
 set -e
 
-git clone --branch MOODLE_41_STABLE --depth 1 https://github.com/moodle/moodle html
+git clone --branch MOODLE_401_STABLE --depth 1 https://github.com/moodle/moodle html
 
 set +e
